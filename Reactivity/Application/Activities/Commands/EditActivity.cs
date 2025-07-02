63e5311bb7a803cdd8954b99b8117386aace7cd8
Mateusz <mateuszbiernat070899@gmail.com>
2025-07-02 19:37:49 +0200

    End Of Section 4

---
+++ Reactivity/Application/Activities/Commands/EditActivity.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities.Commands
+{
+    public class EditActivity
+    {
+
+        public class Command : IRequest
+        {
+            public required Activity Activity { get; set; }
+        }
+
+        public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command>
+        {
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await context.Activities
+                    .FindAsync([request.Activity.Id], cancellationToken)
+                        ?? throw new Exception("Cannot find activity"); // jeśli bedzie null to wyrzuci nam wyjatek
+
+                // activity.Title = request.Activity.Title; // bez użytcia automapera musisz to tak wszystko wypełnić
+                mapper.Map(request.Activity, activity);
+
+                await context.SaveChangesAsync(cancellationToken);
+
+
+            }
+        }
+    }
+}
---
+++ Reactivity/Application/Activities/Queries/GetActivityList.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities.Queries
+{
+    public class GetActivityList
+    {// mediator powinien mieć utworzoną klase Query ktra implementuje interfejs IRequers 
+        public class Query : IRequest<List<Activity>> {  }
+
+        public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>>
+        {
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken) // musimy implementować interfejs!
+            {
+                return await context.Activities.ToListAsync();
+
+            }
+        }
+    }
+}
